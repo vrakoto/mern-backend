ee411668b837a9d980cbb59bbe6f53d2a1656d7f
carellien <carellien.rakotoarisoa@gmail.com>
2024-05-03 12:26:41 +0200

    ajout champ dateHeure dans reunion

--- models/Reunion.js
+++ models/Reunion.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const reunionSchema = new mongoose.Schema({
+    dateHeure: { type: Date, required: true },
     duree: { type: Number, required: true },
     motif: { type: String, required: true },
     id_salle: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'salle' }
